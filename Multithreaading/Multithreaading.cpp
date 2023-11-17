61b05583f17ff7ee2a9c8c56d8e8d750a3336a79
Wiam <meganite647@gmail.com>
2023-11-17 13:53:47 +0400

    Juicy stuff begins! Setup slaves jobs
    - Launching slaves and make sure they done their work!

--- Multithreaading/Multithreaading.cpp
+++ Multithreaading/Multithreaading.cpp
@@ -5,15 +5,19 @@
 #include <cmath>
 #include <limits>
 #include "include/MyTimer.h"
+#include <thread>
 
 constexpr size_t DATASET_SIZE = 5'000'000;
+
+using slave = std::thread;
 
 int main()
 {
     // Define random engine and data set
     std::minstd_rand rne;
     std::vector<std::array<int, DATASET_SIZE>> datasets{4};
-
+    std::vector<slave> slaves;
+    
     MyTimer timer;
     timer.Mark();
     
@@ -28,14 +32,22 @@ int main()
     // Requires a good bit of a cpu power
     for(auto& arr : datasets)
     {
-        for(int x : arr)
+        auto s = [&arr]
         {
-            constexpr auto limit = static_cast<double>(std::numeric_limits<int>::max());
-            const auto y = static_cast<double>(x) / limit;
-            arr[0] += static_cast<int>(std::sin(std::cos(y)) * limit);
-        }
+            for(int x : arr)
+            {
+                constexpr auto limit = static_cast<double>(std::numeric_limits<int>::max());
+                const auto y = static_cast<double>(x) / limit;
+                arr[0] += static_cast<int>(std::sin(std::cos(y)) * limit);
+            } 
+        };
+        slaves.push_back(slave{s});
     }
-    
+    // Make sure all the slaves have done their work
+    for(auto& s : slaves)
+    {
+        s.join();
+    }
     const float t = timer.Peek();
     std::cout << "Processing the datasets took " << t << " seconds" << std::endl;
     return 0;
