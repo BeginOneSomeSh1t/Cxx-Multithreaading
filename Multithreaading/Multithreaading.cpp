c2c180600953e70a76f8bee45dd3c3d04f6cbd3a
Wiam <meganite647@gmail.com>
2023-11-20 13:28:23 +0400

    Solution ! Mutext will solve ya problems! Preventing race conditions on a very base level

--- Multithreaading/Multithreaading.cpp
+++ Multithreaading/Multithreaading.cpp
@@ -6,16 +6,22 @@
 #include <limits>
 #include "include/MyTimer.h"
 #include <thread>
+#include <mutex>
 
 constexpr size_t DATASET_SIZE = 5'000'000;
 
-void ProcessData(std::array<int, DATASET_SIZE>& arr, int& sum)
+void ProcessData(std::array<int, DATASET_SIZE>& arr, int& sum, std::mutex& mtx)
 {
     for(int x : arr)
     {
+        // lock the mutex, not to allow other guys to modify the value
+        mtx.lock();
+        
         constexpr auto limit = static_cast<double>(std::numeric_limits<int>::max());
         const auto y = static_cast<double>(x) / limit;
         sum += static_cast<int>(std::sin(std::cos(y)) * limit);
+        
+        mtx.unlock();
     } 
 }
 
@@ -36,14 +42,15 @@ int main()
         std::ranges::generate(arr, rne);
         
     }
-
+    
+    std::mutex mtx;
     int sum = 0;
     
     
     // Requires a good bit of a cpu power
     for(auto& arr : datasets)
     {
-        threads.push_back(std::thread{ProcessData, std::ref(arr), std::ref(sum)}); // wrapp paramter to transfer the arr across threading dimensions)))
+        threads.push_back(std::thread{ProcessData, std::ref(arr), std::ref(sum), std::ref(mtx)}); // wrapp paramter to transfer the arr across threading dimensions)))
     }
     // Make sure all the slaves have done their work
     for(auto& s : threads)
