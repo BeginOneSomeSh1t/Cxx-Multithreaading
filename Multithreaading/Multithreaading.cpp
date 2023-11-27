a07a56d880470736082ba8821e0e1e717b44fef3
Wiam <meganite647@gmail.com>
2023-11-27 12:10:30 +0400

    Add master control and Workers to do the multithreading stuff

--- Multithreaading/Multithreaading.cpp
+++ Multithreaading/Multithreaading.cpp
@@ -76,6 +76,102 @@ int DoOneBiggie()
     return 0;
 }
 
+// Interface for the main thread
+class MasterControl
+{
+public:
+    MasterControl(int workers_count)
+        :
+    workers_count_{workers_count},
+    lk_{mtx_},
+    done_count_{0}
+    {}
+    
+    void SignalDone()
+    {
+        {
+            std::lock_guard lk{mtx_};
+            ++done_count_;
+        }
+
+        if(done_count_ == workers_count_)
+            cv_.notify_one();
+    }
+    
+    void WaitForAllDone()
+    {
+        cv_.wait(lk_, [this]{return done_count_ == workers_count_;});
+
+        done_count_ = 0;
+    }
+    
+private:
+    std::condition_variable cv_;
+    std::mutex mtx_;
+    std::unique_lock<std::mutex> lk_;
+    int workers_count_;
+    
+    // shared memory
+    int done_count_;
+};
+
+
+// Interface for a seaprate thread (joins automatically)
+class Worker
+{
+public:
+    Worker(const std::shared_ptr<MasterControl>& sp_mctrl)
+        :
+    sp_mctrl_{sp_mctrl},
+    thread_{&Worker::Run_, this}
+    {}
+
+    void SetJob(std::span<int> dataset, int* p_output)
+    {
+        {
+            std::lock_guard lk{mtx_};
+            input_ = dataset;
+            p_output_ = p_output;
+        }
+        cv_.notify_one();
+    }
+    void Kill()
+    {
+        {
+            std::lock_guard lk{mtx_};
+            b_dying = true;
+        }
+        cv_.notify_one();
+    }
+private:
+    void Run_()
+    {
+        std::unique_lock lk{mtx_};
+        while(true)
+        {
+            cv_.wait(lk, [this]{return p_output_ || b_dying; });
+
+            if(b_dying)
+                break;
+
+            ProcessData(input_, *p_output_);
+
+            p_output_ = nullptr;
+            input_ = {};
+            sp_mctrl_->SignalDone();
+        }
+    }
+    
+    std::shared_ptr<MasterControl> sp_mctrl_;
+    std::jthread thread_;
+    std::condition_variable cv_;
+    std::mutex mtx_;
+    
+    std::span<int> input_;
+    int* p_output_ = nullptr;
+    bool b_dying = false;
+};
+
 int DoSmallies()
 {
     auto datasets = GenerateDataSets();
@@ -89,22 +185,32 @@ int DoSmallies()
     
     MyTimer timer;
     timer.Mark();
-    
-    std::vector<std::jthread> threads;
+
+    constexpr int workers_count{4};
+    auto sp_mctrl {std::make_shared<MasterControl>(workers_count)};
+    std::vector<std::unique_ptr<Worker>> p_workers;
+    for(size_t j = 0; j < workers_count; j++)
+    {
+        p_workers.push_back(std::make_unique<Worker>(sp_mctrl));
+    }
     
     constexpr auto subset_size = DATASET_SIZE / 10'000;
     for(size_t i = 0;  i < DATASET_SIZE; i += subset_size)
     {
         for(size_t j = 0; j < 4; j++)
         {
-            threads.push_back(std::jthread{ProcessData, std::span{ &datasets[j][i], subset_size }, std::ref(sum[j].v)}); // wrapp paramter to transfer the arr across threading dimensions)))
+            p_workers[j]->SetJob(std::span{&datasets[j][i], subset_size}, &sum[j].v);
         }
-        threads.clear();
+        sp_mctrl->WaitForAllDone();
     }
 
     const float t = timer.Peek();
     std::cout << "Result is " <<  sum[0].v + sum[1].v + sum[2].v + sum[3].v << std::endl;
     std::cout << "Time taken: " << t << std::endl;
+
+    for(auto& w : p_workers)
+        w->Kill();
+    
     getchar();
     return 0;
 }
