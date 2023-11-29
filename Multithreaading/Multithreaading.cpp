72545d6ce8c257ea42d0754947dd60c720d45348
Wiam <meganite647@gmail.com>
2023-11-29 12:47:15 +0400

    Redo some stuff

--- Multithreaading/Multithreaading.cpp
+++ Multithreaading/Multithreaading.cpp
@@ -7,11 +7,14 @@
 #include "include/MyTimer.h"
 #include <thread>
 #include <span>
+#include <algorithm>
+
 
 // experimental settings
 constexpr size_t WORKER_COUNT = 4;
-constexpr size_t CHUNK_SIZE = 100;
+constexpr size_t CHUNK_SIZE = 1000;
 constexpr size_t CHUNK_COUNT = 100;
+constexpr size_t SUBSET_SIZE = CHUNK_SIZE / WORKER_COUNT;
 constexpr size_t LIGHT_ITERATIONS = 100;
 constexpr size_t HEAVY_ITERATIONS = 1'000;
 constexpr double PROBABILITY_HEAVY = .02;
@@ -23,38 +26,25 @@ static_assert(CHUNK_SIZE % WORKER_COUNT == 0);
 
 struct task
 {
-    double val;
+    unsigned int val;
     bool heavy;
 
-    [[nodiscard]] double process() const
+    [[nodiscard]] unsigned int process() const
     {
         const auto iterations = heavy ? HEAVY_ITERATIONS : LIGHT_ITERATIONS;
-        double result = val;
+        double intermediate =2 * (static_cast<double>(val) / static_cast<double>(std::numeric_limits<unsigned int>::max())) - 1.;
         for(size_t i = 0; i < iterations; i++)
         {
-            result += std::sin(std::cos(val));
+            intermediate += std::sin(std::cos(intermediate));
         }
-        return result;
+        return static_cast<unsigned int>((1. + intermediate) / 2. * static_cast<double>(std::numeric_limits<unsigned int>::max()));
     }
     
 };
 
-void process_data(std::span<int> arr, int& sum)
-{
-    for(auto x : arr)
-    {
-        // lock the mutex, not to allow other guys to modify the value
-        constexpr auto limit = static_cast<double>(std::numeric_limits<int>::max());
-        const auto y = static_cast<double>(x) / limit;
-        sum += static_cast<int>(std::sin(std::cos(y)) * limit);
-        
-    } 
-}
-
 std::vector<std::array<task, CHUNK_SIZE>> generate_data_sets()
 {
     std::minstd_rand rne;
-    std::uniform_real_distribution dist{-1., 1.};
     std::bernoulli_distribution bernouili_dist{ PROBABILITY_HEAVY };
     std::vector<std::array<task, CHUNK_SIZE>> chunks(CHUNK_COUNT);
 
@@ -62,48 +52,11 @@ std::vector<std::array<task, CHUNK_SIZE>> generate_data_sets()
     for(auto& chunk : chunks)
     {
         // Fills each array with random numbers
-        std::ranges::generate(chunk, [&]{return task{ .val = dist(rne), .heavy = bernouili_dist(rne) };});
+        std::ranges::generate(chunk, [&]{return task{ .val = rne(), .heavy = bernouili_dist(rne) };});
         
     }
 
     return chunks;
-}
-
-int do_one_biggie()
-{
-    // Define random engine and data set
-    auto datasets = generate_data_sets();
-    std::vector<std::thread> threads;
-    
-    MyTimer timer;
-    timer.Mark();
-    
-  
-    struct Value
-    {
-        int v = 0;
-        char padding[60];
-    };
-    Value sum[4];
-    
-    
-    // Requires a good bit of a cpu power
-    for(size_t i = 0; i < 4; i++)
-    {
-        threads.emplace_back(std::thread{process_data, std::span{datasets[i]}, std::ref(sum[i].v)}); // wrapp paramter to transfer the arr across threading dimensions)))
-    }
-
-    
-    // Make sure all the slaves have done their work
-    for(auto& s : threads)
-    {
-        s.join();
-    }
-    const float t = timer.Peek();
-    std::cout << "Result is " << (sum[0].v + sum[1].v + sum[2].v + sum[3].v) << std::endl;
-    std::cout << "Time taken: " << t << std::endl;
-    getchar();
-    return 0;
 }
 
 // Interface for the main thread
@@ -159,12 +112,12 @@ public:
     thread_{&worker::run_, this}
     {}
 
-    void set_job(std::span<int> dataset, int* p_output)
+    void set_job(std::span<const task> dataset)
     {
         {
             std::lock_guard lk{mtx_};
             input_ = dataset;
-            p_output_ = p_output;
+            // Reset the accumulation every time a job is set
         }
         cv_.notify_one();
     }
@@ -176,20 +129,30 @@ public:
         }
         cv_.notify_one();
     }
+    unsigned int get_result() const
+    {
+        return accumulation_;
+    }
 private:
+    void process_data_()
+    {
+        for(const auto& t : input_)
+        {
+            accumulation_ += t.process();
+        } 
+    }
     void run_()
     {
         std::unique_lock lk{mtx_};
         while(true)
         {
-            cv_.wait(lk, [this]{return p_output_ || b_dying; });
+            cv_.wait(lk, [this]{return !input_.empty() || b_dying; });
 
             if(b_dying)
                 break;
-
-            process_data(input_, *p_output_);
-
-            p_output_ = nullptr;
+            
+            process_data_();
+            
             input_ = {};
             sp_mctrl_->signal_done();
         }
@@ -200,63 +163,71 @@ private:
     std::condition_variable cv_;
     std::mutex mtx_;
     
-    std::span<int> input_;
-    int* p_output_ = nullptr;
+    // shared memoru
+    std::span<const task> input_;
+    unsigned int accumulation_ = 0;
     bool b_dying = false;
 };
 
-int do_smallies()
+/*
+ * To conduct the experiment we divide one data set in chunks and allow different threads to process each own chunk and then to go to another data set
+ */
+int do_experiment()
 {
-    auto datasets = generate_data_sets();
-  
-    struct Value
-    {
-        int v = 0;
-        char padding[60];
-    };
-    Value sum[4];
+    const auto chunks = generate_data_sets();
     
     MyTimer timer;
     timer.Mark();
 
-    constexpr int workers_count{4};
-    auto sp_mctrl {std::make_shared<master_control>(workers_count)};
+   auto sp_mctrl = std::make_shared<master_control>();
+    
+    if(!sp_mctrl)
+        throw std::exception("Failed to create master_control");
+
     std::vector<std::unique_ptr<worker>> p_workers;
-    for(size_t j = 0; j < workers_count; j++)
+    for(size_t j = 0; j < WORKER_COUNT; j++)
     {
         p_workers.push_back(std::make_unique<worker>(sp_mctrl));
     }
+
     
-    constexpr auto subset_size = DATASET_SIZE / 10'000;
-    for(size_t i = 0;  i < DATASET_SIZE; i += subset_size)
+    for(const auto& chunk : chunks)
     {
-        for(size_t j = 0; j < 4; j++)
+        for(size_t i_subs = 0; i_subs < WORKER_COUNT; i_subs++)
         {
-            p_workers[j]->set_job(std::span{&datasets[j][i], subset_size}, &sum[j].v);
+            p_workers[i_subs]->set_job(std::span{&chunk[i_subs * SUBSET_SIZE], SUBSET_SIZE});
         }
         sp_mctrl->wait_for_all_done();
     }
 
     const float t = timer.Peek();
-    std::cout << "Result is " <<  sum[0].v + sum[1].v + sum[2].v + sum[3].v << std::endl;
+    
+    // Accumlate the overall result.
+    // !! SIDE NOTE - I know there is a thing called std::accumulate but I thought up to this point they included it in ranges but they didn't, so I just said fuck you I am not gonna write this shit.being shit.end again!
+    unsigned int final_result = 0;
+    for(const auto& w : p_workers)
+    {
+        final_result += w->get_result();
+    }
+    std::cout << "Result is " <<  final_result << std::endl;
     std::cout << "Time taken: " << t << std::endl;
 
+    // Kill the workers
     for(auto& w : p_workers)
+    {
         w->kill();
+    }
     
     getchar();
+
     return 0;
 }
-
 
 int main(int argc, char** argv)
 {
     try
     {
-        if(argc > 1 && std::string{argv[1]} == "smol")
-            return do_smallies();
-        
-        return do_one_biggie();
+        return do_experiment();
     }
     catch (...)
     {
